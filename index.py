ebe1d565eb02e1887df3ec56b0d0f1d784630eff
baolam <lam01662052827@gmail.com>
2022-12-21 17:15:13 +0700

    Updating

--- index.py
+++ index.py
@@ -6,9 +6,23 @@ HEALTH = "HEALTH"
 import serial
 import socketio
 import threading
+from telegram import Bot
+from telegram.ext import MessageHandler, Updater, Filters
+from telegram.utils.request import Request
+
+def receive(message, __):
+    print(message.message)
+
+TOKEN_TELEGRAM = "5843674768:AAFKqehrPbVVwOC29i6Qm-TYo2rRfabNZEw"
+request = Request()
+bot = Bot(token = TOKEN_TELEGRAM, request = request)
+updater = Updater(bot=t_bot, use_context = True)
+dp = updater.dispatcher
+dp.add_handler(MessageHandler(filters=Filters.all, callback=receive))
+
 
 client = socketio.Client()
-lora = serial.Serial()
+lora = serial.Serial("/dev/ttyAMA0")
 
 def control_handler(command):
   print(command)
@@ -20,10 +34,18 @@ def client_service():
   except:
     pass
 
+def telegram_service():
+    updater.start_polling()
+    updater.idle()
+
+def lora_service():
+    while True:
+      commands = lora.readline().decode("utf-8") \
+        .replace('\r\n', '')
+      commands = commands.split(';')
+      if commands[0] == HEALTH:
+        lora.write(b'1;okok')
+
 threading.Thread(name="Client service", target=client_service).start()
-while True:
-  commands = lora.readline().decode("utf-8") \
-    .replace('\r\n', '')
-  commands = commands.split(';')
-  if commands[0] == HEALTH:
-    lora.write(b'1;okok')
+threading.Thread(name="Lora service", target=lora_service).start()
+telegram_service()
