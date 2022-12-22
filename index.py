821086e2de85912c9e16fe3ca5d5145c87d7d8f8
baolam <lam01662052827@gmail.com>
2022-12-22 17:29:36 +0700

    Updating

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 ### Nguyên tắc là thiết bị trạm sẽ luôn có wifi
-SERVER = ""
+server = ""
 NAMESPACE = ""
 HEALTH = "HEALTH"
 
@@ -10,16 +10,18 @@ from telegram import Bot
 from telegram.ext import MessageHandler, Updater, Filters
 from telegram.utils.request import Request
 
+update_server = False
 def receive(message, __):
-    print(message.message)
+  global update_server
+  print(message.message)
+  update_server = True
 
 TOKEN_TELEGRAM = "5843674768:AAFKqehrPbVVwOC29i6Qm-TYo2rRfabNZEw"
 request = Request()
 bot = Bot(token = TOKEN_TELEGRAM, request = request)
-updater = Updater(bot=t_bot, use_context = True)
+updater = Updater(bot=bot, use_context = True)
 dp = updater.dispatcher
 dp.add_handler(MessageHandler(filters=Filters.all, callback=receive))
-
 
 client = socketio.Client()
 lora = serial.Serial("/dev/ttyAMA0")
@@ -28,23 +30,26 @@ def control_handler(command):
   print(command)
 
 def client_service():
+  global update_server
   client.on("control", handler=control_handler, namespace=NAMESPACE)
   try:
-    client.connect(SERVER, namespaces=[NAMESPACE])
-  except:
-    pass
+    while not update_server:
+      pass
+    client.connect(server, namespaces=[NAMESPACE])
+  except KeyboardInterrupt as e:
+    client.disconnect()
 
 def telegram_service():
-    updater.start_polling()
-    updater.idle()
+  updater.start_polling()
+  updater.idle()
 
 def lora_service():
-    while True:
-      commands = lora.readline().decode("utf-8") \
-        .replace('\r\n', '')
-      commands = commands.split(';')
-      if commands[0] == HEALTH:
-        lora.write(b'1;okok')
+  while True:
+    commands = lora.readline().decode("utf-8") \
+      .replace('\r\n', '')
+    commands = commands.split(';')
+    if commands[0] == HEALTH:
+      lora.write(b'1;okok')
 
 threading.Thread(name="Client service", target=client_service).start()
 threading.Thread(name="Lora service", target=lora_service).start()
